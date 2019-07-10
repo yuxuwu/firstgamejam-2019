26f09aa78f65eb31e6b9ca355900bc2b43d3162f
Yu Xuan Wu <yuxu.wu1@gmail.com>
2019-07-10 18:21:02 -0400

    Adds Emotion stats

---
+++ Assets/Scripts/Graph/ChoiceEmotionNode.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using XNode;
+
+public class ChoiceEmotionNode : IDNodeBase {
+
+	[Input(backingValue = ShowBackingValue.Never)] [SerializeField] Node Prev;
+	public int Jealousy;
+	[Output(backingValue = ShowBackingValue.Never)] [SerializeField] Node JealousyRoute;
+	public int Pride;
+	[Output(backingValue = ShowBackingValue.Never)] [SerializeField] Node PrideRoute;
+	public int Ambition;
+	[Output(backingValue = ShowBackingValue.Never)] [SerializeField] Node AmbitionRoute;
+
+	[Output(backingValue = ShowBackingValue.Never)] [SerializeField] Node FailRoute;
+
+	public Node GetPrevNode()
+	{
+		NodePort port = null;
+		port = GetOutputPort("Prev");
+		return port.Connection.node;
+	}
+
+	public Node GetNextNodeJealousy()
+	{
+		NodePort port = null;
+		port = GetOutputPort("JealousyRoute");
+		return port.Connection.node;
+	}
+
+	public Node GetNextNodePride()
+	{
+		NodePort port = null;
+		port = GetOutputPort("PrideRoute");
+		return port.Connection.node;
+	}
+
+	public Node GetNextNodeAmbition()
+	{
+		NodePort port = null;
+		port = GetOutputPort("AmbitionRoute");
+		return port.Connection.node;
+	}
+
+	public Node GetNextFail()
+	{
+		NodePort port = null;
+		port = GetOutputPort("FailRoute");
+		return port.Connection.node;
+	}
+
+	public override object GetValue(NodePort port)
+	{
+		return null;
+	}
+}
---
+++ Assets/Scripts/StatsTracker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class StatsTracker : MonoBehaviour
+{
+    public int Jealousy {get; private set;}
+    public int Pride {get; private set;}
+    public int Ambition {get; private set;}
+
+    public void AddJealousy(int val)
+    {
+        Jealousy += val;
+    }
+
+    public void SubJealousy(int val)
+    {
+        Jealousy -= val;
+    }
+
+    public void AddPride(int val)
+    {
+        Pride += val;
+    }
+
+    public void SubPride(int val)
+    {
+        Pride -= val;
+    }
+
+    public void AddAmbition(int val)
+    {
+        Ambition += val;
+    }
+
+    public void SubAmbition(int val)
+    {
+        Ambition -= val;
+    }
+}
